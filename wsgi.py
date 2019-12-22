46a780994db0b745ceb4625632d750285fa64445
Chi Liu <ichase.liu@gmail.com>
2019-12-22 15:24:48 +0800

    Restructure project

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,10 @@ import datetime
 from flask import Flask
 from flask import jsonify
 
-import lib
+from lib import fib
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -14,15 +15,17 @@ def index():
     now = datetime.datetime.now().isoformat()
     return jsonify(message='Hello World!!', ts=now)
 
-@app.route('/ping')
+
+@app.route('/ping/')
 def ping():
     """ping"""
     return 'pong'
 
-@app.route('/fib/<int:n>')
-def fib(n):
+
+@app.route('/fib/seq/<int:n>/')
+def fib_seq(n):
     """Get Fibonacci sequence till F_n."""
     if not 0 <= n <= 1000:
         return jsonify(error='n must between 0 and 1000'), 400
     t = time.time()
-    return jsonify(n=n, seq=lib.fib_seq(n), ms=(time.time() - t) * 1e3)
+    return jsonify(n=n, seq=fib.seq(n), ms=(time.time() - t) * 1e3)
